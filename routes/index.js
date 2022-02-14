22b3b1133165ec6c1ad032fdd67a0403a3a74301
Hema2320 <hemadharsh23@gmail.com>
2022-02-14 14:05:45 +0530

    Task

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/time', function(req, res, next) {
-  res.render('Timestamp');
+router.get('/', function(req, res, next) {
+  res.render('');
 });
 
 
